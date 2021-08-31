da86d8f5335dc188200c60deca26108563ae00a6
Unknown <me@rjchow.com>
2021-09-01 07:24:35 +0800

    Step 4: Add Swap UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Symfoni } from "./hardhat/SymfoniContext";
-import { Greeter } from './components/Greeter';
+import { Swap } from "./components/Swap";
 
 function App() {
-
   return (
     <div className="App">
-      <header className="App-header">
-        <Symfoni autoInit={true} >
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-          <Greeter></Greeter>
-        </Symfoni>
-      </header>
+      <Symfoni autoInit={true}>
+        <div className="min-h-screen bg-gray-800">
+          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
+            <div className="text-gray-100 text-6xl pt-28 pb-10">
+              Swappity Swap
+            </div>
+            <Swap
+              tokenA="0x4C5EB1291EE7982F5562aBA87Dc459f5e76EfdED"
+              tokenB="0x00D3B14F6FE51DCb05b0382D0a5E75e17e1D8EAd"
+            ></Swap>
+          </div>
+        </div>
+      </Symfoni>
     </div>
   );
 }
---
+++ frontend/src/components/Swap.tsx
@@ -0,0 +1,50 @@
+import React, { useContext, useEffect, useState } from "react";
+
+interface Props {
+  tokenA: string;
+  tokenB: string;
+}
+
+export const Swap: React.FC<Props> = ({ tokenA, tokenB }) => {
+  return (
+    <div className="bg-white shadow sm:rounded-lg">
+      <div className="px-4 py-5">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="text-gray-800 text-4xl">Token A</div>
+          <div className="text-gray-800 text-3xl"> to </div>
+          <div className="text-gray-800 text-4xl">Token B</div>
+          <div className="flex justify-center">
+            <span className="flex-item text-gray-800 text-2xl">Amount:</span>
+            <input
+              type="text"
+              name="Amount"
+              id="amount"
+              className="mx-2 flex-item shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block  border-gray-300 rounded-md text-gray-800 text-2xl w-1/6 text-center"
+              placeholder="20"
+            />
+          </div>
+          <div></div>
+          <div className="flex justify-center">
+            <span className="flex-item text-gray-800 text-2xl">Receive:</span>
+            <input
+              type="text"
+              name="Receive"
+              id="receive"
+              disabled
+              className="mx-2 flex-item shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block  border-gray-300 rounded-md text-gray-800 text-2xl w-1/6 text-center"
+              placeholder="20"
+            />
+          </div>
+          <div></div>
+          <div></div>
+          <button
+            type="submit"
+            className="mt-3 inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-gray-800 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          >
+            Swap!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
