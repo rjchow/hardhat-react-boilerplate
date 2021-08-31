aa15e5aea980c7362a99b0452d52995047c796de
Unknown <me@rjchow.com>
2021-09-01 07:41:20 +0800

    Step 5: Add token symbol fetching

--- frontend/src/components/Swap.tsx
+++ frontend/src/components/Swap.tsx
@@ -1,18 +1,44 @@
 import React, { useContext, useEffect, useState } from "react";
-
+import { ERC20Context } from "../hardhat/SymfoniContext";
+import { ERC20 } from "../hardhat/typechain/ERC20";
+import ethers from "ethers";
 interface Props {
   tokenA: string;
   tokenB: string;
 }
 
 export const Swap: React.FC<Props> = ({ tokenA, tokenB }) => {
+  const ERC20Factory = useContext(ERC20Context);
+
+  const [tokenAInstance, setTokenAInstance] = useState<ERC20>();
+  const [tokenBInstance, setTokenBInstance] = useState<ERC20>();
+
+  const [tokenASymbol, setTokenASymbol] = useState<string>();
+  const [tokenBSymbol, setTokenBSymbol] = useState<string>();
+
+
+  useEffect(() => {
+    if (ERC20Factory.instance) {
+      setTokenAInstance(ERC20Factory.instance!.attach(tokenA));
+      setTokenBInstance(ERC20Factory.instance!.attach(tokenB));
+    }
+
+    const fetchTokenSymbols = async () => {
+      if (!tokenAInstance || !tokenBInstance) return;
+
+      setTokenASymbol(await tokenAInstance.symbol());
+      setTokenBSymbol(await tokenBInstance.symbol());
+    };
+    fetchTokenSymbols();
+  }, [ERC20Factory.instance, tokenA, tokenB]);
+
   return (
     <div className="bg-white shadow sm:rounded-lg">
       <div className="px-4 py-5">
         <div className="grid grid-cols-3 gap-4">
-          <div className="text-gray-800 text-4xl">Token A</div>
+          <div className="text-gray-800 text-4xl">{tokenASymbol}</div>
           <div className="text-gray-800 text-3xl"> to </div>
-          <div className="text-gray-800 text-4xl">Token B</div>
+          <div className="text-gray-800 text-4xl">{tokenBSymbol}</div>
           <div className="flex justify-center">
             <span className="flex-item text-gray-800 text-2xl">Amount:</span>
             <input
